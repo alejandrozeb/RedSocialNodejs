40383826cccb1a501734ef1bc6a214df51e7539f
unknown <alejandrojzeballos@gmail.com>
2021-01-24 01:25:23 -0400

    añadiendo la configuracion de handlebars(con helpers) al server

--- imgShare/src/server/config.js
+++ imgShare/src/server/config.js
@@ -1,5 +1,25 @@
+const path = require('path');
+const exphbs= require("express-handlebars");
 module.exports = app =>{
     //settings
     app.set('port', process.env.PORT || 3000);
+    
+    //views
+    app.set('views', path.join(__dirname, 'views' ));
+    app.set(".hbs", exphbs({
+        defaultLayout: 'main',
+        partialsDir: path.join(app.get('views'), 'partials'),
+        layoutsDir: path.join(app.get('views'), 'layouts'),
+        extname: '.hbs',
+        helpers: require('./helpers')
+    }) );
+    app.set('view engine', '.hbs');
+    //middlewares
+
+    //routes
+
+    //erros
+
+
     return app;
 }
---
+++ imgShare/src/server/helpers.js
@@ -0,0 +1,1 @@
+//helpers para handlebars
