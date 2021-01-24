bd12b74317f9bb95eab0b84eb057755d07fc2d0c
unknown <alejandrojzeballos@gmail.com>
2021-01-23 21:57:44 -0400

    instalando las dependencias del proyecto

---
+++ apuntes/comandos.js
@@ -0,0 +1,22 @@
+/*  comandos para el proyecto
+    npm init --yes 
+
+    dependencias
+    express
+    express-handlebars
+    MONGOOSE
+    morgan //servidor para guardar peticiones
+    multer  //imagnes
+    fs-extra //usa async await o promesas
+    errorhandler //nos ayuda con los errores
+    md5 //cifrado de informacion
+    
+
+    npm install express@4.16.4 md5@2.2.1 morgan@1.9.1 multer@1.4.1 fs-extra@7.0.1 mongoose@5.3.14 errorhandler@1.5.0 express-handlebars@5.2.0
+
+
+    usaremos filesystem para los archivos
+    gravatar es un servicio para registrarte con tu correo y podemos consumir la identificacion con eso.
+
+
+*/
