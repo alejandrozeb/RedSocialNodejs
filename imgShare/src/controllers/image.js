cefb83341d932bb44b59400622527cb6c133359c
unknown <alejandrojzeballos@gmail.com>
2021-01-27 02:12:38 -0400

    añadiendo recursividad con la funcion saveImage

--- imgShare/src/controllers/image.js
+++ imgShare/src/controllers/image.js
@@ -2,6 +2,7 @@ const path = require("path");
 const {randomNumber} = require('../helpers/libs');
 const fs =require('fs-extra');
 const {Image} = require("../models");
+const image = require("../models/image");
 
 
 const ctrl = {};
@@ -9,27 +10,34 @@ ctrl.index= (req,res) =>{
     res.send('image pag');
 };
 ctrl.create = async(req,res)  =>{
-    const imgUrl = randomNumber();
-    console.log(imgUrl);
-    //multer hae visible la info
-    const imageTempPath = req.file.path;
-    const ext = path.extname(req.file.originalname).toLowerCase();    //saca la extension de la imagen
-    const targetPath = path.resolve(`src/public/upload/${imgUrl}${ext}`);
+    const saveImage = () =>{
+        const imgUrl = randomNumber();
+        const images =  await image.find({filename: imgUrl});
+        if(images.length>0){
+            saveImage();
+        }else{
+        const imageTempPath = req.file.path;
+        const ext = path.extname(req.file.originalname).toLowerCase();    //saca la extension de la imagen
+        const targetPath = path.resolve(`src/public/upload/${imgUrl}${ext}`);
 
-    if(ext === '.png' || ext === '.jpg' || ext === '.jpeg' || ext === '.gif' ){
-        await fs.rename(imageTempPath, targetPath);
-        const newImg =  new Image({
-            title: req.body.title,
-            filename: imgUrl + ext,
-            description: req.body.description
-        });
-        const imageSaved =  await newImg.save();        
-    } else{
-        await fs.unlink(imageTempPath);
-        res.status(500).json({error: 'Only Images are allowed'});
+        if(ext === '.png' || ext === '.jpg' || ext === '.jpeg' || ext === '.gif' ){
+            await fs.rename(imageTempPath, targetPath);
+            const newImg =  new Image({
+                title: req.body.title,
+                filename: imgUrl + ext,
+                description: req.body.description
+            });
+            const imageSaved =  await newImg.save();        
+        } else{
+            await fs.unlink(imageTempPath);
+            res.status(500).json({error: 'Only Images are allowed'});
+        }
+
+        res.send('works!');
     }
-
-    res.send('works!');
+    };
+    saveImage();   
+    //multer hae visible la info    
 };
 
 ctrl.like = (req,res)  =>{
