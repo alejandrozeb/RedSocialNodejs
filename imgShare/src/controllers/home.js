3e08283d59f930fbe985f621a00a19be6465e853
unknown <alejandrojzeballos@gmail.com>
2021-01-28 02:22:03 -0400

    mostrando las imagenes con handlebars el objeto devuelto debe ser un json y no de tipo documento de mongo

--- imgShare/src/controllers/home.js
+++ imgShare/src/controllers/home.js
@@ -4,7 +4,7 @@ const ctrl = {};
 const {Image} = require("../models");
 
 ctrl.index = async (req,res)  =>{
-    const images = await Image.find().sort({timestamp: 1});
+    const images = await Image.find().sort({timestamp: 1}).lean();
     res.render('index', {images});
 }
 module.exports = ctrl;
