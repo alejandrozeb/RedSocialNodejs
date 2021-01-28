caff8715561c9a3cc0e2cc92259a9992b64c7461
unknown <alejandrojzeballos@gmail.com>
2021-01-28 01:41:08 -0400

    enviando los objetos a la vista

--- imgShare/src/controllers/home.js
+++ imgShare/src/controllers/home.js
@@ -1,5 +1,10 @@
+const image = require("../models/image");
+
 const ctrl = {};
-ctrl.index = (req,res)  =>{
-    res.render('index');
+const {Image} = require("../models");
+
+ctrl.index = async (req,res)  =>{
+    const images = await Image.find().sort({timestamp: 1});
+    res.render('index', {images});
 }
 module.exports = ctrl;
