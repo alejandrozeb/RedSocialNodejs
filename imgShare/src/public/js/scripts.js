8a5cec3f10c5c7c09f38c3c21381967df3d34b08
unknown <alejandrojzeballos@gmail.com>
2021-01-31 01:22:04 -0400

    añadiendo jquery de los likes

--- imgShare/src/public/js/scripts.js
+++ imgShare/src/public/js/scripts.js
@@ -1,1 +1,11 @@
-console.log('reading');
+//con jquery
+console.log('redingd');
+$('#btn-like').click(function (e){
+    e.preventDefault();
+    let imgId = $(this).data('id');
+    
+    $.post('/images' + imgId + '/like')
+        .done(data => {
+            console.log(data);
+        });
+})
