5e2f0ab2eacf72caccc39ca74b8d0d7900581187
unknown <alejandrojzeballos@gmail.com>
2021-01-31 23:19:53 -0400

    añadiendo ajax al delete

--- imgShare/src/public/js/scripts.js
+++ imgShare/src/public/js/scripts.js
@@ -8,4 +8,20 @@ $('#btn-like').click(function (e){
         .done(data => {
             $('.likes-count').text(data.likes);
         });
-})
+});
+
+$('#btn-delete').click(function(e){
+    e.preventDefault();
+    let $this = $(this);
+    const response =  confirm('Are you sure you want to delete this image?');
+    if(response){
+        let imgId = $this.data('id');
+        $.ajax({
+            url: '/images/'+ imgId,
+            type: 'DELETE'
+        })
+        .done(function(result){
+            console.log(result);
+        });
+    }
+});
