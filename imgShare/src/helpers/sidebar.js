57eebb9bb3ad9f7d2bec56b730d7df96338fc46f
unknown <alejandrojzeballos@gmail.com>
2021-02-02 01:42:58 -0400

    añadiendo los comments

--- imgShare/src/helpers/sidebar.js
+++ imgShare/src/helpers/sidebar.js
@@ -1,8 +1,10 @@
 const Stats = require('./stats');
 const images = require('./images');
 const Comments = require('./comments');
+const comments = require('./comments');
 
 module.exports = function(viewModel){
     images.popular();
     Stats();
+    comments.newest();
 }
