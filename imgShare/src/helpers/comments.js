d88ec8d5f460af2545483d68a2b8655227e662b2
unknown <alejandrojzeballos@gmail.com>
2021-02-03 01:22:08 -0400

    añadiendo los comentarios a la vista

--- imgShare/src/helpers/comments.js
+++ imgShare/src/helpers/comments.js
@@ -3,11 +3,12 @@ module.exports = {
     async newest(){
         const comments = await Comment.find()
             .limit(5)
-            .sort({timestamp: -1});
+            .sort({timestamp: -1}).lean();
         for (const comment of comments){
-            const image = await Image.findOne({_id: comment.image_id});
+            const image = await Image.findOne({_id: comment.image_id}).lean();
             comment.image =image;
         }
+        console.log(comments);
 
         return comments;
     }
